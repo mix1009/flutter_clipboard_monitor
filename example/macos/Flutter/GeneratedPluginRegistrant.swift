acac1f71382fd26d302e4e3cd73aa93cdbf81e05
tofutim <tim@gig8.com>
2021-11-27 09:56:05 -0800

    ran flutter create plugin adding macOS

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import clipboard_monitor
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ClipboardMonitorPlugin.register(with: registry.registrar(forPlugin: "ClipboardMonitorPlugin"))
+}
